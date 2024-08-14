d9662cc5c08eff9efda90bb9549131fa1dd4f599
Sam <98758390+blakeles@users.noreply.github.com>
2024-08-14 19:15:45 +0100

    Fixed mobile double tap
    A single touch would result in two presses. This was fixed by stopping the column from being pressed again for 100 milliseconds.

--- scripts/column.gd
+++ scripts/column.gd
@@ -10,9 +10,12 @@ func _ready():
 	circles = get_children().slice(1) # Gets references to the circles without the collision area
 
 func _on_area_2d_input_event(viewport, event, shape_idx):
-	if InputEventMouseButton:
-		if event.is_pressed():
-			column_clicked.emit(self)
+	# Pickable needs to be turned on and off to stop duplicates click on mobile
+	if InputEventMouseButton && event.is_pressed():
+		$'Area2D'.input_pickable = false
+		column_clicked.emit(self)
+		await get_tree().create_timer(0.1).timeout
+		$'Area2D'.input_pickable = true
 			
 func place_circle(colour : String):
 	if circle_index > 5: return false
